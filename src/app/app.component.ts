02a1bb4c015f8416f1eaabd6eff30f41697e6e63
Stiven <strujillo1293@gmail.com>
2018-10-08 02:53:58 -0400

    implementando ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'actividad4';
+  vehiculos:any = [
+    {marca: 'Ferrari'},
+    {marca: 'Lamborghini'},
+    {marca: 'Suzuki'},
+    {marca: 'Volvo'},
+  ]
 }
